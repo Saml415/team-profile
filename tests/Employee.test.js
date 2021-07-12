a2e801c7abcfdee47f196b428334a3a850969dd9
Sam Lloyd <bobdoe784@gmail.com>
2021-07-12 15:53:12 -0700

    Implement Employee constructor

---
+++ tests/Employee.test.js
@@ -0,0 +1,20 @@
+// import the Employee class
+const Employee = require("../lib/Employee");
+
+// create a suite with describe
+
+describe("Employee", () => {
+  describe("constructor", () => {
+    it("sets name, id and email properties", () => {
+      const name = "Israel";
+
+      const employee = new Employee("Israel", 5, "israel@email.com");
+
+      expect(employee).toEqual({
+        name: "Israel",
+        id: 5,
+        email: "israel@email.com",
+      });
+    });
+  });
+});
