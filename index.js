0e79839db6d68c439bebbc6ae52561bfd66d0137
Sam Lloyd <bobdoe784@gmail.com>
2021-07-12 22:07:34 -0700

    object markdown no longer necessary

--- index.js
+++ index.js
@@ -1,28 +1,2 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is your name?",
-      name: "name",
-    },
-    {
-      type: "list",
-      message: "What is you title?",
-      name: "titles",
-      choices: ["Manager", "Engineer", "Intern"],
-    },
-  ])
-  .then((answers) => {
-    console.log(answers);
-    fs.writeFile("index.html", JSON.stringify(answers) + '\n', (error) => {
-      if (error) {
-        console.log(error);
-      }
-    });
-  });
-
-  function mainMenu(){
-
-  }
