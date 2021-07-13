d8e3f67ca4440b1dce6d797a2767d4ca1e24e6ec
Sam Lloyd <bobdoe784@gmail.com>
2021-07-12 19:49:36 -0700

    Intern test has been completed

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,3 +1,24 @@
+const Intern = require("../lib/Intern");
+const intern = new Intern("Felix", 34, "felix@email.com", "UW");
+
 describe("Engineer", () => {
-    it.todo("noting to do yet")
+  describe("constructor", () => {
+    it("sets name, id, email properties and office number", () => {
+      expect(intern).toEqual({
+        name: "Felix",
+        id: 34,
+        email: "felix@email.com",
+        school: "UW",
+      });
+    });
+  });
+
+  describe("getSchool", () => {
+    const result = intern.getSchool();
+    expect(result).toBe("UW");
+  });
+  describe("getRole", () => {
+    const result = intern.getRole();
+    expect(result).toBe("Intern");
+  });
 });
