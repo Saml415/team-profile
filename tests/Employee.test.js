7ed3f57008463e10cd2e06665447917add1205f7
Sam Lloyd <bobdoe784@gmail.com>
2021-07-12 17:25:40 -0700

    Manager and Employee Tests are complete

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,15 +1,11 @@
 // import the Employee class
 const Employee = require("../lib/Employee");
-
+const employee = new Employee("Israel", 5, "israel@email.com");
 // create a suite with describe
 
 describe("Employee", () => {
   describe("constructor", () => {
     it("sets name, id and email properties", () => {
-      const name = "Israel";
-
-      const employee = new Employee("Israel", 5, "israel@email.com");
-
       expect(employee).toEqual({
         name: "Israel",
         id: 5,
@@ -19,10 +15,23 @@ describe("Employee", () => {
   });
 
   describe("getName", () => {
-    const employee = new Employee("Israel", 5, "israel@email.com");
-
     const result = employee.getName();
 
     expect(result).toBe("Israel");
+  });
+  describe("getId", () => {
+    const result = employee.getId();
+
+    expect(result).toBe(5);
+  });
+  describe("getEmail", () => {
+    const result = employee.getEmail();
+
+    expect(result).toBe("israel@email.com");
+  });
+  describe("getRole", () => {
+    const result = employee.getRole();
+
+    expect(result).toBe("Employee");
   });
 });
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,3 +1,19 @@
-describe("Engineer", () => {
-    it.todo("noting to do yet")
+const Manager = require("../lib/Manager");
+const manager = new Manager("Israel", 5, "israel@email.com", 12);
+describe("Manager", () => {
+  describe("constructor", () => {
+    it("sets name, id, email properties and office number", () => {
+      expect(manager).toEqual({
+        name: "Israel",
+        id: 5,
+        email: "israel@email.com",
+        number: 12,
+      });
+    });
+  });
+
+  describe("getRole", () => {
+    const result = manager.getRole();
+    expect(result).toBe("Manager");
+  });
 });
