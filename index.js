009936c108f18e585965b4f773bac1d11a40bba1
Sam Lloyd <bobdoe784@gmail.com>
2021-07-12 22:39:34 -0700

    Manager prompt created and prints object

--- index.js
+++ index.js
@@ -1,2 +1,74 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const makeHTML = require('./src/makeHTML')
+
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const teamArray = []
+const managerPrompt = () => {
+  return inquirer.prompt ([
+      {
+          type: 'input',
+          name: 'name',
+          message: 'Who is the manager of this team?', 
+          
+      },
+      {
+          type: 'input',
+          name: 'id',
+          message: "Please enter the manager's ID.",
+          
+      },
+      {
+          type: 'input',
+          name: 'email',
+          message: "Please enter the manager's email.",
+          
+      },
+      {
+          type: 'input',
+          name: 'officeNumber',
+          message: "Please enter the manager's office number",
+      }
+  ])
+  .then(managerInput => {
+      const  { name, id, email, officeNumber } = managerInput; 
+      const manager = new Manager (name, id, email, officeNumber);
+
+      teamArray.push(manager); 
+      console.log(manager); 
+  })
+};
+
+// const addEmployee = () => {
+//   return inquirer.prompt([
+
+
+
+
+
+
+//   ])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// }
+
+
+  
