0bb3742ab4b872676ccb67df9e869d2170c6fb9e
Sam Lloyd <bobdoe784@gmail.com>
2021-07-12 19:25:40 -0700

    Engineer test full functioning

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,3 +1,22 @@
+const Engineer = require("../lib/Engineer");
+const engineer = new Engineer("Max", 2, "max@email.com", "maxd");
 describe("Engineer", () => {
-    it.todo("noting to do yet")
+  describe("constructor", () => {
+    it("sets name, id, email properties and GitHub account", () => {
+      expect(engineer).toEqual({
+        name: "Max",
+        id: 2,
+        email: "max@email.com",
+        account: "maxd",
+      });
+    });
+  });
+  describe("getGithub", () =>{
+      const result = engineer.account
+      expect(result).toBe("maxd")
+  });
+  describe("getRole",()=>{
+      const result = engineer.getRole();
+      expect(result).toBe("Engineer")
+  })
 });
